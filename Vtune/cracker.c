71d5a37dc7f11b452a53334b0c0528ff471029f3
noobcoder45 <99007904+noobcoder45@users.noreply.github.com>
2023-04-03 15:02:43 +0530

    Update cracker.c
    Update code

--- Vtune/cracker.c
+++ Vtune/cracker.c
@@ -9,10 +9,10 @@
 
 // CONFIGURATION
 // VARIABLE PARAMETER
-#define ITER 10000                  // Maximum number of iterations
+#define ITER 400                  // Maximum number of iterations
 
 // CONSTANT PARAMETERS
-#define MAX_CRYPT_LEN 200         // Maximum length of the ciphertext
+#define MAX_CRYPT_LEN 8000         // Maximum length of the ciphertext
 #define KEY_SIZE 100000
 #define PASS_X_LIM 1000
 #define PASS_Y_LIM 100
@@ -32,61 +32,80 @@ int my_strlen(char *str)
 {
     char *start = str;
     while(*(str++) != '\0');
-    return str-start;
+    return str-start-1;
 }
 
 // Decrypting ciphertext using the found password
 char* DeCrypt(char *ciphertext, char *pswd)
 {
-    int sub_func(int num)
-    {
-        switch (num)
-        {
-            case 0: return 1;
-            case 1: return 3;
-            case 2: return 2;
-            case 3: return 4;
-            case 4: return 7;
-            case 5: return 6;
-            case 6: return 10;
-            case 7: return 5;
-            case 8: return 9;
-            case 9: return 16;
-            case 10: return 15;
-            case 11: return 14;
-            case 12: return 11;
-            case 13: return 17;
-            case 14: return 12;
-            case 31: return 102;
-            default: return -1;
-        }
-    }
+    // int sub_func(int num)
+    // {
+    //     switch (num)
+    //     {
+    //         case 0: return 1;
+    //         case 1: return 3;
+    //         case 2: return 2;
+    //         case 3: return 4;
+    //         case 4: return 7;
+    //         case 5: return 6;
+    //         case 6: return 10;
+    //         case 7: return 5;
+    //         case 8: return 9;
+    //         case 9: return 16;
+    //         case 10: return 15;
+    //         case 11: return 14;
+    //         case 12: return 11;
+    //         case 13: return 17;
+    //         case 14: return 12;
+    //         // case 31: return 102;
+    //         default: return -1;
+    //     }
+    // }
 
-    char* buff = (char*)malloc(my_strlen(ciphertext));
-    memcpy(buff, ciphertext, my_strlen(ciphertext));
-
-    int px = 0, py = 0;
-
-    do {
-        int px = 0;
-        do {
-            int p = px * PASS_Y_LIM + py;
-            struct key_attr key_i = key[p];
-            
-            int prf_int = (key_i.li ^ key_i.zi ^ key_i.ki) & (15);
-            int mask = key_i.a[(prf_int+1) >> 4];
-            
-            if(prf_int == 15)
-            {
-                prf_int = 14;
-            }
-            
-            int sub_result = sub_func(prf_int & mask) ^ pswd[p];
-
-            buff[p % my_strlen(ciphertext)] ^= sub_result;
-        } while ((++px < PASS_X_LIM));
-    } while((++py < PASS_Y_LIM));
+    int len=my_strlen(ciphertext);
+    char* buff = (char*)malloc(len+1);
+    memcpy(buff, ciphertext, len+1);
     
+    int j=0;
+    int i=0;
+    while(i < PASS_X_LIM * PASS_Y_LIM)
+    {
+        struct key_attr key_i = key[i];
+        int prf_int = (key_i.li ^ key_i.zi ^ key_i.ki) & 15;
+        int sub_result;
+        if(prf_int==15){
+          sub_result=102^pswd[i];
+        }
+        else{
+            switch (prf_int)
+        {
+            case 0: prf_int = 1; break;
+            case 1: prf_int = 3; break;
+            case 2: prf_int = 2; break;
+            case 3: prf_int = 4; break;
+            case 4: prf_int = 7; break;
+            case 5: prf_int = 6; break;
+            case 6: prf_int = 10; break;
+            case 7: prf_int = 5; break;
+            case 8: prf_int = 9; break;
+            case 9: prf_int = 16; break;
+            case 10: prf_int = 15; break;
+            case 11: prf_int = 14; break;
+            case 12: prf_int = 11; break;
+            case 13: prf_int = 17; break;
+            case 14: prf_int = 12; break;
+            // case 31: prf_int = 102; break;
+            default: prf_int = -1;
+        }
+          sub_result=prf_int^pswd[i];
+        }
+        if(j==len){
+            j=0;
+        }
+        buff[j] ^= sub_result;
+        j++;
+        i++;
+    }
     return buff;
 }
 
@@ -99,43 +118,27 @@ int my_memcmp(char *dst, char *src, unsigned count)
 // Checking the password CRC
 int CheckCRC(char *plaintext, int validCRC)
 {
-    // Calculating the password checksum
-    int CalculateCRC(char *plaintext)
-    {
-        int a;
-        int x = -1; // CRC calculation error
-
-        for (a = 0; a < my_strlen(plaintext); a++)
-            x += *(int *)(plaintext + a);
-        
-        free(plaintext);
-        
-        return x;
-    }
-
-    if ((CalculateCRC(plaintext) == validCRC) && (my_memcmp(plaintext, correct_plaintext, my_strlen(plaintext))))
-        return 1;
+    int a;
+    int x = -1; // CRC calculation error
+    int len=my_strlen(plaintext);
+    for (a = 0; a < len; a++)
+    {    x += *(int *)(plaintext + a);}
+    if ((x == validCRC) && (!my_memcmp(plaintext, correct_plaintext, len)))
+    { return 1;}
     return 0;
 }
 
 // Processing the current password
 void do_pswd(char *ciphertext, char *pswd, int validCRC, int progress)
 {
-  char *buff;
-
-  // Checking the password CRC
-  if (CheckCRC(DeCrypt(ciphertext, pswd), validCRC))
+  char *buff=DeCrypt(ciphertext, pswd);
+  if (CheckCRC(buff, validCRC))
   { 
-    // CRC match
-    // Decrypting
-    buff = DeCrypt(ciphertext, pswd);
-    buff[10] = '\0'; // Restricting size of buffer printed
-
-    // Displaying the decryption results
+    buff[10] = '\0';
     printf("CRC %8X: try to decrypt: \"%s\"\n",
            CheckCRC(pswd, validCRC), buff);
-    free(buff);
   }
+    free(buff);
 }
 
 // DO NOT MODIFY ANY CODE BELOW
@@ -153,6 +156,7 @@ int gen_pswd(char *ciphertext, char *pswd, int max_iter, int validCRC)
 
         // * Main loop of password generation *
         // Using the "counter" algorithm
+
         while ((++pswd[p]) > 'z')
         {
             pswd[p] = '!';
@@ -172,9 +176,8 @@ int gen_pswd(char *ciphertext, char *pswd, int max_iter, int validCRC)
 // Displaying the number, using DOT as a delimiter
 void print_dot(float per)
 {
-
 // * * * CONFIGURATION * * *
-#define N 3        // Separating three positions
+#define N 3       // Separating three positions
 #define DOT_SIZE 1 // Size of the DOT delimiter
 #define DOT ","    // Delimiter
   int a;
@@ -182,7 +185,6 @@ void print_dot(float per)
   sprintf(buff, "%0.0f", per);
   for (a = my_strlen(buff) - N; a > 0; a -= N)
   {
-
     memmove(buff + a + DOT_SIZE, buff + a, 66);
     if (buff[a] == ' ')
       break;
